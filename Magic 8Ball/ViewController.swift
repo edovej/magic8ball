86744e8bce54ac2595a29031bb694e7387908a86
Edin Vejzagic <edoje2008@hotmail.com>
2020-02-11 16:05:19 +0100

    Initial Commit

---
+++ Magic 8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8Ball
+//
+//  Created by Edin Vejzagic on 11/2/20.
+//  Copyright © 2020 Edin Vejzagic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
